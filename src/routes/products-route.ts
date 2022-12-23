4755e5a74bed14678d3df9f91b7a8764bf1adc58
Maksim Laurouski <maximlavrovsky@mail.ru>
2022-12-23 15:39:26 +0300

    feat express validator, and added repositories

--- src/routes/products-route.ts
+++ src/routes/products-route.ts
@@ -1,49 +1,49 @@
 import {Request, Response, Router} from 'express';
+import {productsRepository} from '../repositories/products-repository';
+import {body} from 'express-validator';
+import {inputValidationMiddleware} from '../middlewares/input-validation-middleware';
 
-const products = [{id: 1, title: 'tomato'}, {id: 2, title: 'orange'}]
 
 export const productsRoute = Router({})
 
+const titleValidation = body('title').trim().isLength({
+    min: 1,
+    max: 15
+}).withMessage('title should be min 1 max 15 symbols')
+
 productsRoute.get('/', (req: Request, res: Response) => {
-    if (req.query.title) {
-        let searchString = req.query.title.toString()
-        res.send(products.filter(p => p.title.indexOf(searchString) > -1))
-    } else {
-        res.send(products)
-    }
+    const foundProducts = productsRepository.findProducts(req.query.title?.toString() && null)
+    res.send(foundProducts)
 })
-productsRoute.post('/', (req: Request, res: Response) => {
-    const newProduct = {
-        id: products.length + 1,
-        title: req.body.title
-    }
-    products.push(newProduct)
-    res.status(201).send(newProduct)
-})
+productsRoute.post('/', titleValidation, inputValidationMiddleware,
+    (req: Request, res: Response) => {
+        const newProduct = productsRepository.createProduct(req.body.title)
+        res.status(201).send(newProduct)
+    })
+productsRoute.put('/:id', titleValidation, inputValidationMiddleware,
+    (req: Request, res: Response) => {
+        const isUpdated = productsRepository.updateProduct(+req.params.id, req.body.title)
+        if (isUpdated) {
+            const product = productsRepository.findProductById(+req.params.id)
+            res.send(product)
+        } else {
+            res.send(404)
+        }
+    })
 productsRoute.get('/:id', (req: Request, res: Response) => {
-    let product = products.find(p => p.id === +req.params.id)
+    let product = productsRepository.findProductById(+req.params.id)
     if (product) {
         res.send(product)
     } else {
         res.send(404)
     }
 })
-productsRoute.put('/:id', (req: Request, res: Response) => {
-    let product = products.find(p => p.id === +req.params.id)
-    if (product) {
-        product.title = req.body.title
-        res.status(202).send(product)
+productsRoute.delete('/:id', (req: Request, res: Response) => {
+    const isDeleted = productsRepository.deleteProduct(+req.params.id)
+    if (isDeleted) {
+        res.send(204)
     } else {
         res.send(404)
     }
 })
-productsRoute.delete('/:id', (req: Request, res: Response) => {
-    for (let i = 0; i < products.length; i++) {
-        if (products[i].id === +req.params.id) {
-            products.splice(i, 1)
-            res.send(204)
-            return
-        }
-    }
-    res.send(404)
-})
+
